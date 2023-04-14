08966c136ad76378f934c8e5d577a071f3b7cac8
Juan david <jdlopez2013@misena.edu.co>
2023-04-14 11:23:49 -0500

    first commit

--- app/src/main/java/com/example/recyclerviewexamplo/PersonaProvider.kt
+++ app/src/main/java/com/example/recyclerviewexamplo/PersonaProvider.kt
@@ -20,7 +20,7 @@ class PersonaProvider {
                 "Administrativo"
             ),
             Persona(
-                "Jaime Martinez Miranda Monsalve",
+                "Jaime Martinez Miranda ",
                 "Aporte en sitio",
                 "Edward_17@outlook.com",
                 "https://i.ibb.co/mG82K0N/Jaime-Martinez-Miranda-Monsalve-Soporte.png",
@@ -68,12 +68,52 @@ class PersonaProvider {
                 "Administrativo"
             ),
             Persona(
-                "Aracely García Ospina",
-                "psicóloga",
-                "AgarciaO@sena.edu.co",
-                "https://i.ibb.co/C5cbNHB/Aracely-Garc-Ia-Ospina-psicologa.png",
-                "Asistencia Psicológica",
+                "Angela María alzate ",
+                "Asistente Administrativo",
+                "AAlzate@sena.edu.co",
+                "https://i.ibb.co/vmwPj8C/Angela-Maria-Alzate-Betancur-asistenteadministrativo.png",
+                "apoyo a actividades administrativas y al subdirector del centro",
                 "Administrativo"
+            ),
+            Persona(
+                "Diego Fernando Calderón ",
+                "contratación de personal",
+                "dfcalderonr@sena.edu.co",
+                "https://i.ibb.co/4WBtD7v/Diego-Fernando-Calderon.png",
+                "líder de contratación de servicios personales",
+                "Administrativo"
+            ),
+            Persona(
+                "Isabel Cristina Monsalve ",
+                "acompañamiento a contratación ",
+                "isemonsalve@sena.edu.co",
+                "https://i.ibb.co/X24mWzx/Isabel-Cristina-Monsalve-Profecional-Apoyo.png",
+                "profesional de apoyo y contratación de servicios personales",
+                "Administrativo"
+            ),
+            Persona(
+                "Luz marina Vanegas Samper",
+                "Profesional biblioteca",
+                "",
+                "https://img.freepik.com/iconos-gratis/icono-perfil-usuario_318-33925.jpg",
+                "Liderar procesos y procedimientos de biblioteca del CIAA (centro innovación agroindustria y aviación)",
+                "Administrativo"
+            ),
+            Persona(
+                "Lina Marcela Quiceno moreno ",
+                "Apoyo administrativo",
+                "Lmquiceno@sena.edu.co",
+                "https://i.ibb.co/P43G7DG/Lina-Marcela-Quiceno-Moreno-Apoyo-Administrativo.png",
+                "Apoyar el proceso de programación a instructores, a programación, agendas y aprobaciones de plataforma",
+                "Administrativo"
+            ),
+            Persona(
+                "Monica Serna",
+                "Guardia de seguridad",
+                "Lmquiceno@sena.edu.co",
+                "https://img.freepik.com/iconos-gratis/icono-perfil-usuario_318-33925.jpg",
+                "Brindar protección",
+                "Logistica"
             ),
         )
     }
